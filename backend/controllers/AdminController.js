73c11997906c85282e7abc33b3802936f0e5c56b
Gautham <gauthamskaniyattil@gmail.com>
2023-12-02 02:07:30 +0530

    started doing web tokens

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -1,5 +1,10 @@
 //Admin Login
 const bcrypt = require(`bcrypt`);
+const jwt = require(`jsonwebtoken`);
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const AdminLogin = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
--- backend/models/AdminModel.js
+++ backend/models/AdminModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const AdminSchema = Schema({
@@ -13,5 +13,21 @@ const AdminSchema = Schema({
     require: true,
   },
 });
+
+//Login user
+
+AdminSchema.statics.login = function (email, password) {
+  if (!email || !password) {
+    throw Error("Either Field is empty!");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Not a Valid Email");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Not a Strong password try adding Numbers|uppercase|special characters"
+    );
+  }
+};
 
 module.exports = mongoose.model("admin", AdminSchema);
