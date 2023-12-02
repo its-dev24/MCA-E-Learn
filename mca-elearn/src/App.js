4c872d78e5ba1a88bb9aed22a8dc5d28c9f2aabe
Gautham <gauthamskaniyattil@gmail.com>
2023-12-02 20:23:04 +0530

    Changes to App

--- mca-elearn/src/App.js
+++ mca-elearn/src/App.js
@@ -14,16 +14,24 @@ import Semester from "./Notes/Semester";
 import Module from "./Notes/Modules";
 import Contributions from "./Notes/Contributions";
 import Notes from "./Notes/Notes";
+import AdminLayout from "./Admin/AdminLayout/AdminLayout";
+import AdminLogin from "./Admin/AdminLogin/AdminLogin";
 
 const myRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<UserDashboard />} />
-      <Route path="/Semester" element={<Semester />} />
-      <Route path="/Subjects/:semester" element={<Subjects />} />
-      <Route path="/Modules/:semester/:subjectCode" element={<Module />} />
-      <Route path="/Notes/:semester/:subjectCode/:module" element={<Notes />} />
-      <Route path="/Contributions" element={<Contributions />} />
+    <Route>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<UserDashboard />} />
+        <Route path="/Semester" element={<Semester />} />
+        <Route path="/Subjects/:semester" element={<Subjects />} />
+        <Route path="/Modules/:semester/:subjectCode" element={<Module />} />
+        <Route
+          path="/Notes/:semester/:subjectCode/:module"
+          element={<Notes />}
+        />
+        <Route path="/Contributions" element={<Contributions />} />
+      </Route>
+      <Route path="/admin" element={<AdminLogin />}></Route>
     </Route>
   )
 );
